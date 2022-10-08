e24268a777efdbf0d8459c26b9ecbc359c582870
Joshua R Licona <joshuajapan@hotmail.com>
2022-10-08 01:10:42 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import './App.css';
-import Title from './Title'
-import Sensor1 from './buttons/sensor1'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-        <Title/>
-
         <p>
-          This is the GUI for our data
+          MQTT Frontend
         </p>
-
-        <Sensor1>Sensor1</Sensor1>
-      </header>
       
+      </header>
     </div>
   );
 }
